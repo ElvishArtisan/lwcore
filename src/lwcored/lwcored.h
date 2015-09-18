921c57cd2397e193cf2c74486ac4dd9e982d4d83
Fred Gleason <fredg@paravelsystems.com>
2015-09-18 15:23:55 -0400

    2015-09-18 Fred Gleason <fredg@paravelsystems.com> 	* Initial package creation.

---
+++ src/lwcored/lwcored.h
@@ -0,0 +1,36 @@
+// lwcored.h
+//
+// lwcored(8) facility router daemon
+//
+//   (C) Copyright 2015 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#ifndef LWCORED_H
+#define LWCORED_H
+
+#include <QObject>
+
+#define LWCORED_USAGE "[options]\n"
+
+class MainObject : public QObject
+{
+ Q_OBJECT;
+ public:
+  MainObject(QObject *parent=0);
+};
+
+
+#endif  // LWCORED_H
